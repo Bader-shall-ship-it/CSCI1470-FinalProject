fe85eb2faed9b80a570a35fec37171320bfaaa92
jfrausto7 <jacob_frausto@brown.edu>
2021-11-23 14:50:29 -0700

    created init method for NTXent

--- models/losses.py
+++ models/losses.py
@@ -1,4 +1,3 @@
-from torch._C import Module
 import torch.nn as nn
 
 
@@ -10,8 +9,11 @@ def accuracy():
 
 class NTXent(nn.Module):
   """ Normalized Temperature-scaled Cross Entropy loss (contrastive) that has support for distributed training with data parallelism """
-  def __init__(self) -> None:
-      pass
 
-  def forward():
+  def __init__(self, batch_size, temperature=0.5) -> None:
+    super(NTXent, self).__init__()
+    self.batch_size = batch_size
+    self.temp = temperature
+
+  def forward(self, e_i, e_j):
     pass
