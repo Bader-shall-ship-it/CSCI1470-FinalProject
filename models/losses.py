a2daab209c895bab02973aebd1879a1c7b8c3175
jfrausto7 <jacob_frausto@brown.edu>
2021-11-22 21:44:38 -0700

    created losses.py with outline of contrastive loss implementation

---
+++ models/losses.py
@@ -0,0 +1,17 @@
+from torch._C import Module
+import torch.nn as nn
+
+
+def gather():
+  pass
+
+def accuracy():
+  pass
+
+class NTXent(nn.Module):
+  """ Normalized Temperature-scaled Cross Entropy loss (contrastive) that has support for distributed training with data parallelism """
+  def __init__(self) -> None:
+      pass
+
+  def forward():
+    pass
