de19688fab14c6bcc7c29b1af1da248cb0ce7d6b
Bader <bader_alabdulrazzaq@brown.edu>
2021-11-30 16:59:36 -0500

    option to scale color jitter

--- data/utils.py
+++ data/utils.py
@@ -3,12 +3,11 @@ import torchvision.transforms as transforms
 import PIL
 from typing import Tuple, List
  
-def generate_augmented_pair(img: PIL.Image, crop_resize: int, normalize_mean: List[float], normalize_std: List[float]) -> Tuple[torch.Tensor, torch.Tensor]:
-    s = 1.0
+def generate_augmented_pair(img: PIL.Image, crop_resize: int, normalize_mean: List[float], normalize_std: List[float], scale: float = 1.0) -> Tuple[torch.Tensor, torch.Tensor]:
     tr = transforms.Compose([
         transforms.RandomResizedCrop(crop_resize),
         transforms.RandomHorizontalFlip(p=0.5),
-        transforms.RandomApply([transforms.ColorJitter(0.8*s, 0.8*s, 0.8*s, 0.2*s)], p=0.8),
+        transforms.RandomApply([transforms.ColorJitter(0.8*scale, 0.8*scale, 0.8*scale, 0.2*scale)], p=0.8),
         transforms.RandomGrayscale(p=0.2),
         transforms.GaussianBlur(kernel_size=0.1*img.height),
         transforms.ToTensor(),
