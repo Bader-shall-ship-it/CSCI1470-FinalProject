8e16522ab54fbea44f8ceec1da9717bf4f020d89
Bader <bader_alabdulrazzaq@brown.edu>
2021-12-02 16:26:14 -0500

    create subsets of dataloader

--- data/utils.py
+++ data/utils.py
@@ -2,7 +2,8 @@ import torch
 import torchvision.transforms as transforms
 import PIL
 from typing import Tuple, List
- 
+from torch.utils.data import DataLoader
+
 def generate_augmented_pair(img: PIL.Image, crop_resize: int, normalize_mean: List[float], normalize_std: List[float], scale: float = 1.0) -> Tuple[torch.Tensor, torch.Tensor]:
     tr = transforms.Compose([
         transforms.RandomResizedCrop(crop_resize),
@@ -21,3 +22,12 @@ def transform_test_img(img: PIL.Image, normalize_mean: List[float], normalize_st
         transforms.Normalize(mean=normalize_mean, std=normalize_std)])
     
     return tr(img)
+
+def create_dataloader_subset(dataloader: DataLoader, train_size: int, test_size: int, batch_size: int = 64) -> Tuple[DataLoader, DataLoader]:
+    train_dataset = torch.utils.data.Subset(dataloader.dataset, torch.arange(train_size))
+    test_dataset = torch.utils.data.Subset(dataloader.dataset, torch.arange(train_size, test_size))
+
+    new_train = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=False, num_workers=2, drop_last=True)
+    new_test = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2, drop_last=True)
+    
+    return new_train, new_test
