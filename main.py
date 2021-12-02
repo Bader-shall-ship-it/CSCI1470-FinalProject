38243dc80e0e1444ae824291dc99346a85e3e5e6
David Young <dichlorodiphen@gmail.com>
2021-12-02 13:40:40 -0500

    Add logic for testing in main.py (untested)

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import argparse
 import os
 import sys
+from typing import Tuple
 
 import numpy as np
 import torch
 import torchvision
+from torch.utils.data import DataLoader
+from analysis import benchmark_simclr_backbone
 
 from data.dataloaders import CIFAR10_dataloader, ImageNet_dataloader
 from models.losses import NTXent
@@ -21,10 +24,11 @@ def parse_args() -> argparse.Namespace:
     )
 
     parser.add_argument(
-        '--action',
-        required=True,
+        '--train',
+        default=True,
         help='Whether to train or to test',
-        choices=['train', 'test'],
+        type=bool,
+        action='store_true',
         dest='action'
     )
 
@@ -66,17 +70,39 @@ def parse_args() -> argparse.Namespace:
         dest='noaug'
     )
 
+    parser.add_argument(
+        '--weights',
+        default='',
+        help='Path for the weights to use in testing',
+        dest='weights'
+    )
+
     return parser.parse_args()
 
 
-def train() -> None:
-    """Helper function to store logic for train action."""
-    pass
+def load_data(train: bool) -> Tuple[DataLoader, DataLoader, bool]:
+    """Loads data for either training or testing."""
+    # Create dataloaders
+    if (args.data == "cifar"):
+        print("Loading cifar dataset")
+        train_loader, test_loader = CIFAR10_dataloader(
+            ~args.noaug, args.batch_size)
+        using_cifar = True
+        num_classes = 10
+    elif (args.data == "imagenet"):
+        print("Loading imagenet dataset")
+        train_loader, test_loader = ImageNet_dataloader(
+            ~args.noaug, args.batch_size)
+        using_cifar = False
+        # TODO: no idea how many classes imagenet 2012 has but idt we using it so its should be ok
+        num_classes = 1000
 
+    # Reduce dastaset if testing
+    if (~train):
+        train_loader = train_loader[:((int)(0.01 * len(train_loader)))]
+        test_loader = test_loader[:((int)(0.01 * len(test_loader)))]
 
-def test() -> None:
-    """Helper function to store logic for test action."""
-    pass
+    return train_loader, test_loader, using_cifar, num_classes
 
 
 def main(args: argparse.Namespace) -> None:
@@ -85,32 +111,32 @@ def main(args: argparse.Namespace) -> None:
     print(f"Using device: {active_device}")
 
     # Load data
-    if (args.data == "cifar"):
-        print("Loading cifar dataset")
-        train_loader, test_loader = CIFAR10_dataloader(~args.noaug, args.batch_size)
-        using_cifar = True
-    elif (args.data == "imagenet"):
-        print("Loading imagenet dataset")
-        train_loader, test_loader = ImageNet_dataloader(
-            ~args.noaug, args.batch_size)
-        using_cifar = False
+    train_loader, test_loader, using_cifar, num_classes = load_data(args.train)
 
-    # Instantiate model
-    model = SimCLRModel(CIFAR=using_cifar, device=active_device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    loss_fn = NTXent(args.batch_size, active_device, tau=1)
+    if (train):
+        # Train
+        model = SimCLRModel(CIFAR=using_cifar, device=active_device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+        loss_fn = NTXent(args.batch_size, active_device, tau=1)
 
-    checkpoint_path = "./checkpoints/"
-    if not os.path.exists(checkpoint_path):
-        os.makedirs(checkpoint_path, exist_ok=True)
+        checkpoint_path = "./checkpoints/"
+        if not os.path.exists(checkpoint_path):
+            os.makedirs(checkpoint_path, exist_ok=True)
 
-    # Train
-    for epoch in range(1, args.epochs + 1):
-        print("Now on epoch " + str(epoch) + "/" + str(args.epochs))
-        train(model, train_loader, optimizer, active_device, loss_fn)
+        # Train
+        for epoch in range(1, args.epochs + 1):
+            print("Now on epoch " + str(epoch) + "/" + str(args.epochs))
+            train(model, train_loader, optimizer, active_device, loss_fn)
 
-        torch.save(model.state_dict(), checkpoint_path +
-                   "model_{}.pth".format(epoch))
+            torch.save(model.state_dict(), checkpoint_path +
+                       "model_{}.pth".format(epoch))
+    else:
+        # Test
+        if (args.weights == ''):
+            print("Weights not specified for testing")
+        else:
+            benchmark_simclr_backbone(train_loader, test_loader, num_classes=num_classes,
+                                      simclr_weights_path=args.weights, cifar=using_cifar, device=active_device)
 
 
 if __name__ == "__main__":
